e6e5dbf3e911efd1e80954a9e54eb48930817c01
ferpasri <ferpasri@gmail.com>
2021-01-12 16:55:54 +0100

    explore navMesh example
    - Randomly select a visible NavMeash node to move and explore labrecruits level

---
+++ iv4XR/src/eu/testar/iv4xr/actions/commands/labActionExploreNavMesh.java
@@ -0,0 +1,127 @@
+/***************************************************************************************************
+ *
+ * Copyright (c) 2020 - 2021 Universitat Politecnica de Valencia - www.upv.es
+ * Copyright (c) 2020 - 2021 Open Universiteit - www.ou.nl
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are met:
+ *
+ * 1. Redistributions of source code must retain the above copyright notice,
+ * this list of conditions and the following disclaimer.
+ * 2. Redistributions in binary form must reproduce the above copyright
+ * notice, this list of conditions and the following disclaimer in the
+ * documentation and/or other materials provided with the distribution.
+ * 3. Neither the name of the copyright holder nor the names of its
+ * contributors may be used to endorse or promote products derived from
+ * this software without specific prior written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+ * AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+ * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+ * ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR CONTRIBUTORS BE
+ * LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+ * CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+ * SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+ * INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
+ * CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+ * ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+ * POSSIBILITY OF SUCH DAMAGE.
+ *******************************************************************************************************/
+
+package eu.testar.iv4xr.actions.commands;
+
+import java.util.Random;
+
+import org.fruit.alayer.Action;
+import org.fruit.alayer.Role;
+import org.fruit.alayer.Roles;
+import org.fruit.alayer.SUT;
+import org.fruit.alayer.State;
+import org.fruit.alayer.TaggableBase;
+import org.fruit.alayer.Tags;
+import org.fruit.alayer.exceptions.ActionFailedException;
+
+import communication.agent.AgentCommand;
+import communication.system.Request;
+import environments.LabRecruitsEnvironment;
+import eu.testar.iv4xr.enums.IV4XRtags;
+import helperclasses.datastructures.Vec3;
+import pathfinding.TriangleGraph;
+
+public class labActionExploreNavMesh extends TaggableBase implements Action {
+	private static final long serialVersionUID = 885421289057696013L;
+
+	private LabRecruitsEnvironment labRecruitsEnvironment;
+	private String agentId;
+	private Vec3 targetPosition;
+
+	public String getAgentId() {
+		return agentId;
+	}
+
+	public void selectedByAgent() {
+		this.set(IV4XRtags.agentAction, true);
+	}
+
+	public labActionExploreNavMesh(State state, LabRecruitsEnvironment labRecruitsEnvironment, String agentId, boolean agentAction, boolean newByAgent) {
+		this.set(Tags.Role, Roles.System);
+		this.set(Tags.OriginWidget, state);
+		this.labRecruitsEnvironment = labRecruitsEnvironment;
+		this.agentId = agentId;
+		this.set(Tags.Desc, toShortString());
+		this.set(IV4XRtags.agentAction, agentAction);
+		this.set(IV4XRtags.newActionByAgent, newByAgent);
+	}
+
+	@Override
+	public void run(SUT system, State state, double duration) throws ActionFailedException {
+		// Move randomly using NavMesh
+		
+		// we need to use the pathfinder graph, if i'm not wrong this maps indexes and vec3
+		TriangleGraph navGraph = labRecruitsEnvironment.pathFinder.graph;
+		
+		// indexes, but we need to obtain Vec3 of these indexes using the graph
+		int[] visibleNavigationNodes = labRecruitsEnvironment.observe(agentId).visibleNavigationNodes;
+		System.out.println("labActionExploreNavMesh visibleNavigationNodes : " + visibleNavigationNodes.length);
+		for(int i = 0; i < visibleNavigationNodes.length; i++) {
+			System.out.println("VisibleNode " + visibleNavigationNodes[i] + " : " + navGraph.toVec3(visibleNavigationNodes[i]));
+		}
+		
+		int random = new Random().nextInt(visibleNavigationNodes.length);
+		System.out.println("Random Node Selected " + visibleNavigationNodes[random] + " : " + navGraph.toVec3(visibleNavigationNodes[random]));
+		
+		targetPosition = navGraph.toVec3(visibleNavigationNodes[random]);
+		labRecruitsEnvironment.moveToward(agentId, currentAgentPosition(), targetPosition);
+	}
+
+	@Override
+	public String toShortString() {
+		return "Agent: " + agentId + " explore NavMesh  from: " + currentAgentPosition() + " to: " + targetPosition;
+	}
+
+	/**
+	 * Two labActionExploreNavMesh are equals if the same agent tries to move to the North
+	 * from the coordinates of a nearby position
+	 */
+	public boolean actionEquals(labActionExploreNavMesh action) {
+		return (this.agentId.equals(action.getAgentId()) 
+				&& this.currentAgentPosition().distance(action.currentAgentPosition()) < 0.2);
+	}
+
+	@Override
+	public String toParametersString() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public String toString(Role... discardParameters) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	private Vec3 currentAgentPosition() {
+		return labRecruitsEnvironment.getResponse(Request.command(AgentCommand.doNothing(agentId))).agent.position;
+	}
+
+}
