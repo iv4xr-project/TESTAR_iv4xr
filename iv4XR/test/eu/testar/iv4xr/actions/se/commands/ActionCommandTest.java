bccbdc0dcb28079982ee137e141b0d682560ac28
Fernando Pastor <32359126+ferpasri@users.noreply.github.com>
2022-07-12 17:26:41 +0200

    update se plugin and add some unit tests
    - Move SE plugin dependency to jitpack
    - Add some JUnit Tests to validate that the connection with SE game works (Disabled by default)

---
+++ iv4XR/test/eu/testar/iv4xr/actions/se/commands/ActionCommandTest.java
@@ -0,0 +1,171 @@
+package eu.testar.iv4xr.actions.se.commands;
+
+import static org.junit.Assert.*;
+
+import org.fruit.alayer.Action;
+import org.fruit.alayer.SUT;
+import org.fruit.alayer.SUTBase;
+import org.fruit.alayer.StdState;
+import org.fruit.alayer.StdWidget;
+import org.fruit.alayer.exceptions.SystemStopException;
+import org.junit.Assume;
+import org.junit.BeforeClass;
+import org.junit.Ignore;
+import org.junit.Test;
+
+import eu.testar.iv4xr.enums.IV4XRtags;
+import spaceEngineers.controller.Observer;
+import spaceEngineers.controller.SpaceEngineers;
+import spaceEngineers.controller.SpaceEngineersJavaProxyBuilder;
+import spaceEngineers.model.Vec2F;
+import spaceEngineers.model.Vec3F;
+
+/**
+ * JUnit tests ignored by default, 
+ * uncomment @Ignore label to test the connection with Space Engineers game + iv4xr-plugin
+ */
+
+@Ignore
+public class ActionCommandTest {
+
+	private static SUT system;
+
+	@BeforeClass
+	public static void setup() {
+		// We only execute this unit tests in windows environments.
+		Assume.assumeTrue(System.getProperty("os.name").toLowerCase().contains("windows"));
+		// If Space Engineers game is running with the iv4xr-plugin enabled
+		SpaceEngineers seController = new SpaceEngineersJavaProxyBuilder().localhost("you");
+		// Empty SUT to attach the controller
+		system = new SUTBase() {
+			@Override
+			public void stop() throws SystemStopException {}
+			@Override
+			public void setNativeAutomationCache() {}
+			@Override
+			public boolean isRunning() {return false;}
+			@Override
+			public String getStatus() {return null;}
+		};
+		system.set(IV4XRtags.iv4xrSpaceEngineers, seController);
+		system.set(IV4XRtags.iv4xrSpaceEngCharacter, seController.getCharacter());
+		system.set(IV4XRtags.iv4xrSpaceEngItems, seController.getItems());
+	}
+
+	@Test
+	public void move_back() {
+		Observer obs = system.get(IV4XRtags.iv4xrSpaceEngineers).getObserver();
+		Vec3F originalPos = obs.observe().getPosition();
+
+		Action move = new seActionCommandMove(new StdWidget(), "you", new Vec3F(0, 0, 1f), 100); // Move to back
+		move.run(system, new StdState(), 0);
+
+		Vec3F newPos = obs.observe().getPosition();
+
+		assertTrue(originalPos != newPos);
+	}
+
+	@Test
+	public void move_front() {
+		Observer obs = system.get(IV4XRtags.iv4xrSpaceEngineers).getObserver();
+		Vec3F originalPos = obs.observe().getPosition();
+
+		Action move = new seActionCommandMove(new StdWidget(), "you", new Vec3F(0, 0, -1f), 100); // Move to front
+		move.run(system, new StdState(), 0);
+
+		Vec3F newPos = obs.observe().getPosition();
+
+		assertTrue(originalPos != newPos);
+	}
+
+	@Test
+	public void move_right() {
+		Observer obs = system.get(IV4XRtags.iv4xrSpaceEngineers).getObserver();
+		Vec3F originalPos = obs.observe().getPosition();
+
+		Action move = new seActionCommandMove(new StdWidget(), "you", new Vec3F(1f, 0, 0), 100); // Move to Right
+		move.run(system, new StdState(), 0);
+
+		Vec3F newPos = obs.observe().getPosition();
+
+		assertTrue(originalPos != newPos);
+	}
+
+	@Test
+	public void move_left() {
+		Observer obs = system.get(IV4XRtags.iv4xrSpaceEngineers).getObserver();
+		Vec3F originalPos = obs.observe().getPosition();
+
+		Action move = new seActionCommandMove(new StdWidget(), "you", new Vec3F(-1f, 0, 0), 100); // Move to Left
+		move.run(system, new StdState(), 0);
+
+		Vec3F newPos = obs.observe().getPosition();
+
+		assertTrue(originalPos != newPos);
+	}
+
+	@Test
+	public void rotate_right() {
+		Observer obs = system.get(IV4XRtags.iv4xrSpaceEngineers).getObserver();
+		Vec3F originalOrientation = obs.observe().getOrientationForward();
+
+		Action rotate = new seActionCommandRotate(new StdWidget(), "you", new Vec2F(0, 500f)); // Rotate to Right
+		rotate.run(system, new StdState(), 0);
+
+		Vec3F newOrientation = obs.observe().getOrientationForward();
+
+		assertTrue(originalOrientation != newOrientation);
+	}
+
+	@Test
+	public void rotate_left() {
+		Observer obs = system.get(IV4XRtags.iv4xrSpaceEngineers).getObserver();
+		Vec3F originalOrientation = obs.observe().getOrientationForward();
+
+		Action rotate = new seActionCommandRotate(new StdWidget(), "you", new Vec2F(0, -500f)); // Rotate to Left
+		rotate.run(system, new StdState(), 0);
+
+		Vec3F newOrientation = obs.observe().getOrientationForward();
+
+		assertTrue(originalOrientation != newOrientation);
+	}
+
+	@Test
+	public void open_helmet() {
+		Observer obs = system.get(IV4XRtags.iv4xrSpaceEngineers).getObserver();
+
+		Action helmet = new seActionCommandHelmetOpen(new StdWidget(), "you");
+		helmet.run(system, new StdState(), 0);
+
+		assertTrue(!obs.observe().getHelmetEnabled());
+	}
+
+	@Test
+	public void close_helmet() {
+		Observer obs = system.get(IV4XRtags.iv4xrSpaceEngineers).getObserver();
+
+		Action helmet = new seActionCommandHelmetClose(new StdWidget(), "you");
+		helmet.run(system, new StdState(), 0);
+
+		assertTrue(obs.observe().getHelmetEnabled());
+	}
+
+	@Test
+	public void use_grinder() {
+		Action grinder = new seActionCommandGrinder(new StdWidget(), "you");
+		grinder.run(system, new StdState(), 0);
+	}
+
+	@Test
+	public void use_welder() {
+		Action welder = new seActionCommandWelder(new StdWidget(), "you");
+		welder.run(system, new StdState(), 0);
+	}
+
+	@Test
+	public void place_block() {
+		Action place = new seActionCommandPlaceBlock(new StdWidget(), "you", "LargeBlockArmorBlock");
+		place.run(system, new StdState(), 0);
+	}
+
+}
---
+++ iv4XR/test/eu/testar/iv4xr/se/ConnectSpaceEngineersTest.java
@@ -0,0 +1,64 @@
+package eu.testar.iv4xr.se;
+
+import static org.junit.Assert.*;
+
+import org.junit.Assume;
+import org.junit.BeforeClass;
+import org.junit.Ignore;
+import org.junit.Test;
+
+import environments.SeEnvironment;
+import eu.iv4xr.framework.mainConcepts.TestAgent;
+import spaceEngineers.controller.ContextControllerWrapper;
+import spaceEngineers.controller.SpaceEngineers;
+import spaceEngineers.controller.SpaceEngineersJavaProxyBuilder;
+import spaceEngineers.controller.SpaceEngineersTestContext;
+import spaceEngineers.model.CharacterObservation;
+import spaceEngineers.model.Observation;
+import uuspaceagent.UUSeAgentState;
+
+/**
+ * JUnit tests ignored by default, 
+ * uncomment @Ignore label to test the connection with Space Engineers game + iv4xr-plugin
+ */
+
+@Ignore
+public class ConnectSpaceEngineersTest {
+
+	private static SpaceEngineers seController;
+
+	@BeforeClass
+	public static void setup() {
+		// We only execute this unit tests in windows environments.
+		Assume.assumeTrue(System.getProperty("os.name").toLowerCase().contains("windows"));
+		// If Space Engineers game is running with the iv4xr-plugin enabled
+		seController = new SpaceEngineersJavaProxyBuilder().localhost("you");
+	}
+
+	@Test
+	public void character_observation() {
+		CharacterObservation characterObs = seController.getObserver().observe();
+		assertTrue(characterObs != null);
+	}
+
+	@Test
+	public void blocks_observation() {
+		Observation blocksObs = seController.getObserver().observeBlocks();
+		assertTrue(blocksObs != null);
+	}
+
+	@Test
+	public void create_agent() {
+		UUSeAgentState stateGrid = new UUSeAgentState("you");
+		SpaceEngineersTestContext context = new SpaceEngineersTestContext();
+		ContextControllerWrapper controllerWrapper = new ContextControllerWrapper(seController, context);
+		// WorldId is empty because we are going to connect to a running level, not load a new one
+		SeEnvironment sEnv = new SeEnvironment("", controllerWrapper);
+		// Finally create the TestAgent
+		TestAgent testAgent = new SeAgentTESTAR("you", "explorer").attachState(stateGrid).attachEnvironment(sEnv);
+
+		assertTrue(testAgent.getId() == "you");
+		assertTrue(testAgent.env() == sEnv);
+	}
+
+}
